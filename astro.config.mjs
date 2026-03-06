ab0d31d7bd5890508e9e36176af73125c2db21e6
Sharqawy Wael <sherqo@sherqo.me>
2026-03-06 16:55:15 +0200

    copied from sherqo.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   site: 'https://caie23p.netlify.app',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
   },
 });
-
